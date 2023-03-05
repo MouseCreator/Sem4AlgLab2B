492623b8ea891e2acc7be4951c9acaf0171d6508
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-03-05 21:30:46 +0200

    Implemented split method

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -6,11 +6,14 @@ public class TreeNode {
     private LinkedList<TreeNode> children;
     private TreeNode parent;
     private boolean isLeaf;
+
+    private int degree;
     private final int minKeys;
     private final int maxKeys;
 
     public TreeNode(int d) {
         minKeys = d - 1;
+        this.degree = d;
         maxKeys = 2 * d - 1;
         this.children = new LinkedList<>();
         this.values = new LList(maxKeys);
@@ -23,23 +26,36 @@ public class TreeNode {
     }
 
 
-
     public void add(double val) {
         if (isLeaf) {
             this.values.add(val);
         } else {
             int toChild = values.position(val);
             if (this.children.get(toChild).isFull()) {
-                this.children.get(toChild).split();
+                this.children.get(toChild).split(this);
             }
             children.get(toChild).add(val);
         }
     }
 
-    private TreeNode[] split() {
+    private void split(TreeNode parent) {
         LList[] lists = this.values.split();
-        TreeNode[] nodes = new TreeNode[3];
-        return nodes; //TODO: implement splitting
+        this.values = lists[0];
+        parent.values.merge(lists[1]);
+        TreeNode neighbor = new TreeNode(isLeaf, lists[2], degree);
+        if (!isLeaf) {
+            for (int i = 0; i < degree; i++) {
+                neighbor.children.add(this.children.remove(0));
+            }
+        }
+    }
+
+    private TreeNode(boolean isLeaf, LList list, int d) {
+        this.isLeaf = isLeaf;
+        this.values = list;
+        minKeys = d - 1;
+        this.degree = d;
+        maxKeys = 2 * d - 1;
     }
 
     private boolean isFull() {
