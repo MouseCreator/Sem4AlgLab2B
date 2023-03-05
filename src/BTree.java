cf069fdb06ec0701def1f1a270f2c5804a708ec7
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-03-05 12:07:11 +0200

    Changed array to array list

--- src/BTree.java
+++ src/BTree.java
@@ -3,18 +3,30 @@ import java.util.NoSuchElementException;
 public class BTree {
     private TreeNode root;
 
+    private int degree;
     public TreeNode getRoot() {
         return root;
     }
     public BTree() {
+        this.degree = 3;
     }
-    public BTree(double[] values) {
-
+    public BTree(int degree) {
+        this.degree = degree;
+    }
+    public BTree(int degree, double[] values) {
+        this.degree = degree;
+        for (double d : values) {
+            this.add(d);
+        }
     }
     public void add(double val) {
-
+        if (root == null) {
+            root = new TreeNode(degree, val);
+        } else {
+            root.add(val);
+        }
     }
-    public void pop(double val) {
+    public void pop(double val) throws NoSuchElementException {
 
     }
 
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,21 +1,39 @@
+import java.util.ArrayList;
+
 public class TreeNode {
-    private double[] val;
-    private TreeNode[] children;
+    private ArrayList<Double> values;
+    private ArrayList<TreeNode> children;
     private TreeNode parent;
     private boolean isLeaf;
 
     private int degree;
 
-    private final int minDegree;
-    private final int maxDegree;
+    private final int minKeys;
+    private final int maxKeys;
 
     public TreeNode(int d) {
-        minDegree = d -1;
-        maxDegree = 2 * d - 1;
+        minKeys = d - 1;
+        maxKeys = 2 * d - 1;
+        this.children = new ArrayList<>();
+        this.values = new ArrayList<>();
+    }
+
+
+    public TreeNode(int degree, double val) {
+        this(degree);
+        this.values.add(val);
     }
 
     private boolean compare(double a, double b) {
         return Math.abs(a - b) < 0.0001;
     }
 
+    public void add(double val) {
+    }
+    public void pop(double val) {
+
+    }
+    public void find(double val) {
+
+    }
 }
