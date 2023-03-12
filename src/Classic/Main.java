717e6b10a6923b884b310dce57a75787bdfbc25a
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-03-12 21:23:43 +0200

    Added main documentation

--- src/Classic/Main.java
+++ src/Classic/Main.java
@@ -3,16 +3,18 @@ package Classic;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Основний клас (запуск програми)
+ */
 public class Main {
     public static void main(String[] args) {
         try {
-            ArrayList<Double> doubles = InputReader.readFile();
-            System.out.println(doubles);
-            BTree tree = new BTree(InputReader.readDegree());
+            ArrayList<Double> doubles = InputReader.readFile(); // зчитування значень
+            BTree tree = new BTree(InputReader.readDegree()); //створення порожнього дерева
             for (double d : doubles) {
-                tree.add(d);
+                tree.add(d); //додавання значень до дерева
             }
-            OutputWriter.write(tree.asString());
+            OutputWriter.write(tree.asString()); //запис дерева у файл
         } catch (IOException e) {
             System.err.println("Cannot open a file");
         }
